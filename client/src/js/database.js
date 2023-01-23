76ab48d569f97f3c1da0a3cfee9fc1969f4fd2c5
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-01-23 11:07:29 -0600

    Finishing to do in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Used class example from activity 24 for this section
+export const putDb = async (content) => 
+{
+  const jateDatabase = await openDB('jate', 1);
+  const tx = jateDatabase.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
+
 
 initdb();
