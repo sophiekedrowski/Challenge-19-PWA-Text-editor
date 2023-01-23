cd1d39fad5ed5b2e92f91f1a01e756fa8c65ad08
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-01-23 09:48:49 -0600

    Added the plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. (Done, had to look at class example and ask someone for the colors)
 // TODO: Add CSS loaders and babel to webpack. (Done, pulled from docs)
 
 module.exports = () => {
@@ -23,13 +23,30 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }), 
     ],
 
     module: {
